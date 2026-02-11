8d736851941051fc4607e6ddf854def8014d3964
johnnygcz <johnny@masonstructures.co.uk>
2026-02-11 13:35:14 +0000

    Create calculateDealScore.js

---
+++ utils/calculateDealScore.js
@@ -0,0 +1,6 @@
+// DEPRECATED: This file is no longer used
+// All deal scoring now uses the unified useDealScoring hook
+// See src/generated/hooks/useDealScoring.js for the single source of truth
+
+// Minimal export to satisfy module system
+export const deprecated = true;
